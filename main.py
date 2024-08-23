070643fe91173a5cc03c6c4f1899e7f614818f55
SystemX <sven22812122007@gmail.com>
2024-08-23 12:30:27 +0800

    feet: добавь базу данных для сохранения данных
    Была добавлена база данных, так как наблюдалась проблема с хранением данных.

--- main.py
+++ main.py
@@ -1,40 +1,25 @@
+from datetime import datetime
 from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, EmailField, SelectField
 from wtforms.validators import DataRequired, Length, Optional
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_KEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
-news = [{'title': 'Удивительное событие в школе',
-         'text': 'Вчера в местной школе произошло удивительное событие - все '
-                 'ученики одновременно зевнули на уроке математики. '
-                 'Преподаватель был так поражен этим коллективным зевком, '
-                 'что решил отменить контрольную работу.'},
-        {'title': 'Случай в зоопарке',
-         'text': 'В зоопарке города произошел необычный случай - ленивец '
-                 'решил не лениться и взобрался на самое высокое дерево в '
-                 'своем вольере. Посетители зоопарка были поражены такой '
-                 'активностью и начали снимать ленивца на видео. В итоге он '
-                 'получил свой собственный канал на YouTube, где он размещает '
-                 'свои приключения.'},
-        {'title': 'Самый красивый пёс',
-         'text': 'Сегодня в парке прошел необычный конкурс - "Самый красивый '
-                 'пёс". Участники конкурса были так красивы, что судьи не '
-                 'могли выбрать победителя. В итоге, конкурс был объявлен '
-                 'ничейным, а участники получили награды за участие, '
-                 'в том числе - пакетики конфет и игрушки в виде косточек. '
-                 'Конкурс вызвал большой интерес у посетителей парка, '
-                 'и его решили повторить в более масштабном формате.'},
-        {'title': 'Новость 3',
-         'text': 'Сегодня в парке прошел необычный конкурс - "Самый красивый '
-                 'пёс". Участники конкурса были так красивы, что судьи не '
-                 'могли выбрать победителя. В итоге, конкурс был объявлен '
-                 'ничейным, а участники получили награды за участие, '
-                 'в том числе - пакетики конфет и игрушки в виде косточек. '
-                 'Конкурс вызвал большой интерес у посетителей парка, '
-                 'и его решили повторить в более масштабном формате.'}
-        ]
+db = SQLAlchemy(app)
+
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+db.create_all()
 
 
 class FeedbackForm(FlaskForm):
@@ -46,30 +31,26 @@ class FeedbackForm(FlaskForm):
 
 
 class NewsForm(FlaskForm):
-    title = StringField(
-        'Название', validators=[DataRequired(message="Поле не должно быть пустым"),
-                                Length(max=255, message='Введите заголовок длинной до 255 символов')])
-    text = TextAreaField(
-        'Текст',
-        validators=[DataRequired(message="Поле не должно быть пустым")])
+    title = StringField('Название',
+                        validators=[DataRequired(message="Поле не должно быть пустым"),
+                                    Length(max=255, message='Введите заголовок длинной до 255 символов')])
+    text = TextAreaField('Текст',
+                         validators=[DataRequired(message="Поле не должно быть пустым")])
     submit = SubmitField('Добавить')
 
 
 @app.route('/')
 def index():
+    news_list = News.query.all()
     return render_template('index.html',
-                           news=news)
+                           news=news_list)
 
 
 @app.route('/news_detail/<int:id>')
 def news_detail(id):
-    title = news[id]['title']
-    text = news[id]['text']
-    return render_template(
-        'news_detail.html',
-        title=title,
-        text=text
-    )
+    news_detail = News.query.get(id)
+    return render_template('news_detail.html',
+                           item=news_detail)
 
 
 @app.route('/feedback/', methods=['GET', 'POST'])
@@ -89,10 +70,12 @@ def feedback():
 def add_news():
     form = NewsForm()
     if form.validate_on_submit():
-        title = form.title.data
-        text = form.text.data
-        news.append({'title': title, 'text': text})
-        return redirect(url_for('index'))
+        news = News()
+        news.title = form.title.data
+        news.text = form.text.data
+        db.session.add(news)
+        db.session.commit()
+        return redirect(url_for('index', id=news.id))
     return render_template('add_news.html', form=form)
 
 
